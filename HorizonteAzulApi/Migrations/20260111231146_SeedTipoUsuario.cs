8db8640b0a29ada353209b8db80467b3013c3554
Giovanne da Silva Batista <giobatista14@gmail.com>
2026-01-11 19:45:50 -0400

    Seed inicial para TipoUsuario via migration EF Core
    Adiciona migração SeedTipoUsuario que limpa, reseta o identity e insere os tipos de usuário 'SUPERADMINISTRADOR', 'ADMINISTRADOR' e 'USUARIO' na tabela TipoUsuario. Inclui também o snapshot do modelo atualizado. O método Down remove os registros e reseta o identity.

---
+++ HorizonteAzulApi/Migrations/20260111231146_SeedTipoUsuario.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HorizonteAzulApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedTipoUsuario : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"DELETE TipoUsuario
+
+                                   DBCC CHECKIDENT ('TipoUsuario', RESEED, 0);
+
+                                   INSERT INTO TipoUsuario (Descricao) VALUES ('SUPERADMINISTRADOR')
+                                   INSERT INTO TipoUsuario (Descricao) VALUES ('ADMINISTRADOR')
+                                   INSERT INTO TipoUsuario (Descricao) VALUES ('USUARIO')");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"DELETE TipoUsuario
+                                   DBCC CHECKIDENT ('TipoUsuario', RESEED, 0)");
+        }
+    }
+}
