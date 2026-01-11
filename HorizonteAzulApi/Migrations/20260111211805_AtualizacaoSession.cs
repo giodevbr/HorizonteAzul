d1d9a1160d59f81ab9076407a0e6bb98d7a3075f
Giovanne Batista <giobatista14@gmail.com>
2026-01-11 17:19:38 -0400

    Adiciona campos detalhados à entidade Sessao e migration
    Inclui novas propriedades string em Sessao para armazenar dados como IPs, navegador, protocolo, tipo de conteúdo, entre outros. Cria a migration AtualizacaoSession para adicionar essas colunas à tabela Sessao no banco de dados e atualiza o ModelSnapshot para refletir as mudanças no modelo.

---
+++ HorizonteAzulApi/Migrations/20260111211805_AtualizacaoSession.cs
@@ -0,0 +1,128 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HorizonteAzulApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class AtualizacaoSession : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ConteudoTipo",
+                table: "Sessao",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Metodo",
+                table: "Sessao",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Navegador",
+                table: "Sessao",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "NavegadorAgente",
+                table: "Sessao",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "NavegadorTipo",
+                table: "Sessao",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "NavegadorVersao",
+                table: "Sessao",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Plataforma",
+                table: "Sessao",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Protocolo",
+                table: "Sessao",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Server",
+                table: "Sessao",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ServidorIp",
+                table: "Sessao",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "UsuarioIp",
+                table: "Sessao",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ConteudoTipo",
+                table: "Sessao");
+
+            migrationBuilder.DropColumn(
+                name: "Metodo",
+                table: "Sessao");
+
+            migrationBuilder.DropColumn(
+                name: "Navegador",
+                table: "Sessao");
+
+            migrationBuilder.DropColumn(
+                name: "NavegadorAgente",
+                table: "Sessao");
+
+            migrationBuilder.DropColumn(
+                name: "NavegadorTipo",
+                table: "Sessao");
+
+            migrationBuilder.DropColumn(
+                name: "NavegadorVersao",
+                table: "Sessao");
+
+            migrationBuilder.DropColumn(
+                name: "Plataforma",
+                table: "Sessao");
+
+            migrationBuilder.DropColumn(
+                name: "Protocolo",
+                table: "Sessao");
+
+            migrationBuilder.DropColumn(
+                name: "Server",
+                table: "Sessao");
+
+            migrationBuilder.DropColumn(
+                name: "ServidorIp",
+                table: "Sessao");
+
+            migrationBuilder.DropColumn(
+                name: "UsuarioIp",
+                table: "Sessao");
+        }
+    }
+}
--- HorizonteAzulApi/Models/HorizonteAzul/Sessao.cs
+++ HorizonteAzulApi/Models/HorizonteAzul/Sessao.cs
@@ -8,6 +8,17 @@
         public string Token { get; set; }
         public DateTime DataCriacao { get; set; }
         public DateTime DataExpiracao { get; set; }
+        public string UsuarioIp { get; set; }
+        public string ServidorIp { get; set; }
+        public string Metodo { get; set; }
+        public string Protocolo { get; set; }
+        public string ConteudoTipo { get; set; }
+        public string Plataforma { get; set; }
+        public string Server { get; set; }
+        public string Navegador { get; set; }
+        public string NavegadorTipo { get; set; }
+        public string NavegadorVersao { get; set; }
+        public string NavegadorAgente { get; set; }
 
         public virtual Usuario Usuario { get; set; }
     }
